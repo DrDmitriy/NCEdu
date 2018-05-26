80fd259beb951355b9f9e780716dfd816f0fc1a6
DrDmitriy <dr_dmitriy96@mail.ru>
2018-05-26 14:32:32 +0300

    QuickSort NCEdu

---
+++ quickSort/src/main/java/ru/ncedu/DmDrozhzhin/Main.java
@@ -0,0 +1,17 @@
+package ru.ncedu.DmDrozhzhin;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+        Integer[] array = QuickSort.generateRandomArr(100);
+        int first = 0;
+        int last = array.length-1;
+
+        QuickSort.qSort(array,first,last);
+        System.out.println("");
+        System.out.println(Arrays.toString(array));
+        System.out.println(QuickSort.isCorrectSort(array));
+    }
+
+}
---
+++ quickSort/src/main/java/ru/ncedu/DmDrozhzhin/QuickSort.java
@@ -0,0 +1,61 @@
+package ru.ncedu.DmDrozhzhin;
+import java.util.Random;
+
+public class QuickSort {
+
+    public static Integer[] generateRandomArr(int arrayLenght){
+        Random random = new Random();
+        Integer[] array = new Integer[arrayLenght];
+
+        for (int i = 0; i <array.length;i++){
+            array[i] = random.nextInt(10000);
+            System.out.print(array[i]+" ");
+        }
+
+        return array;
+    }
+
+    public static void qSort(Integer[] array, int first, int last){
+        if (array.length==0) {return;}
+        if (first>=last){return;}
+        int i = first;
+        int j = last;
+        int mid = array[first+(last-first)/2];
+        while (i<=j){
+            while (array[i]<mid){i++;}
+            while (array[j]>mid){j--;}
+
+            if (i<=j) {
+                int temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+                i++;
+                j--;
+            }
+        }
+        if(first<j){
+            qSort(array,first,j);
+        }
+        if(last>i) {
+            qSort(array, i, last);
+        }
+    }
+
+    public static boolean isCorrectSort(Integer[] arr){
+        try {
+            for (int i = 0; i < arr.length-1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        catch (NullPointerException e){
+            System.out.println("isCorrectSort return "+e.toString());
+          return false;
+        }
+    }
+
+
+
+}
