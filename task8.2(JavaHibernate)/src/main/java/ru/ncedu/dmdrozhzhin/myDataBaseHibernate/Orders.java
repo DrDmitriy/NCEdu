f898a0f0024b8df612d431d4d6ac5dae2e9d338a
DrDmitriy <dr_dmitriy96@mail.ru>
2018-07-17 12:41:03 +0300

    2

---
+++ task8.2(JavaHibernate)/src/main/java/ru/ncedu/dmdrozhzhin/myDataBaseHibernate/Orders.java
@@ -0,0 +1,8 @@
+package ru.ncedu.dmdrozhzhin.myDataBaseHibernate;
+
+public class Orders {
+    private int order_id;
+    private int product_id;
+    private double price;
+    private int customer_id;
+}
---
+++ task8.2(JavaHibernate)/src/main/java/ru/ncedu/dmdrozhzhin/myDataBaseHibernate/Products.java
@@ -0,0 +1,14 @@
+package ru.ncedu.dmdrozhzhin.myDataBaseHibernate;
+
+import java.util.Date;
+
+public class Products {
+    private int product_id;
+    private int manufacturer_id;
+    private String name;
+    private double price;
+    private String category;
+    private boolean is_available;
+    private int year_production;
+    private String size;
+}
